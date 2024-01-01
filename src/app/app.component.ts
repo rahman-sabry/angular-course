5b13ea514260871312ebf4530a9145dd818fbb6a
Abdul Rahman <amansab96@gmail.com>
2024-01-01 15:33:42 +0800

    Angular Built-In Pipes - Complete Catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  price = 9.992366223466;
+
+  rate = 0.68;
+
+  startDate = new Date(2000, 0, 1)
+
   onCourseSelected(course:Course) {
 
     console.log("App component - click event bubbled ...", course);
